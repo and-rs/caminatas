1883da598f172d1a4da4048292f72076c715de1c
and-rs <deserd@protonmail.com>
2025-08-11 21:31:26 -0500

    chore: clean up of unused files

--- app/api/pages.py
+++ app/api/pages.py
@@ -1,33 +1,34 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-from app.data.data import walks
+from app.data.walks import walks
 
-mainRouter = APIRouter()
+router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
 
-@mainRouter.get("/", response_class=HTMLResponse)
+
+@router.get("/", response_class=HTMLResponse)
 async def home_section(request: Request):
     return templates.TemplateResponse("pages/home.jinja", {"request": request})
 
 
-@mainRouter.get("/about", response_class=HTMLResponse)
+@router.get("/about", response_class=HTMLResponse)
 async def about_section(request: Request):
     return templates.TemplateResponse("pages/about.jinja", {"request": request})
 
 
-@mainRouter.get(path="/walks", response_class=HTMLResponse)
+@router.get(path="/walks", response_class=HTMLResponse)
 async def walks_section(request: Request):
     context = {"request": request, "walks": walks}
     return templates.TemplateResponse("pages/walks.jinja", context)
 
 
-@mainRouter.get(path="/blog", response_class=HTMLResponse)
+@router.get(path="/blog", response_class=HTMLResponse)
 async def blog_section(request: Request):
     return templates.TemplateResponse("pages/blog.jinja", {"request": request})
 
 
-@mainRouter.get("/json", response_class=JSONResponse)
+@router.get("/json", response_class=JSONResponse)
 async def json_example():
     return {"Hello": "World"}
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
+# pyright: reportUnknownMemberType=false
 import arel
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.config import DEBUG
-from app.router.api import main
+from app.api import pages
 
 app = FastAPI(title="Caminatas")
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -20,9 +21,12 @@ if DEBUG:
     app.add_websocket_route(
         "/hot-reload", route=hot_reload_wrapper, name="hot-reload"
     )
+
+    # annoying warnings related to unknow types from libraries
     app.add_event_handler("startup", hot_reload.startup)
     app.add_event_handler("shutdown", hot_reload.shutdown)
     templates.env.globals["DEBUG"] = True
     templates.env.globals["hot_reload"] = hot_reload
 
-app.include_router(main.mainRouter)
+app.include_router(pages.router)
+
