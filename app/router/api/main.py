5782433b6229078d8fd9de9ef2c05d3fd20702b7
David <daperezcruz@gmail.com>
2025-08-11 21:18:58 -0500

    [refactor] | refactor home, componetize walks page

--- app/router/api/main.py
+++ app/router/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+from app.data.data import walks
 
 mainRouter = APIRouter()
 
@@ -18,12 +19,14 @@ async def about_section(request: Request):
 
 @mainRouter.get(path="/walks", response_class=HTMLResponse)
 async def walks_section(request: Request):
-    return templates.TemplateResponse("pages/walks.jinja", {"request": request})
+    context = {"request": request, "walks": walks}
+    return templates.TemplateResponse("pages/walks.jinja", context)
 
 
 @mainRouter.get(path="/blog", response_class=HTMLResponse)
 async def blog_section(request: Request):
     return templates.TemplateResponse("pages/blog.jinja", {"request": request})
+
 
 @mainRouter.get("/json", response_class=JSONResponse)
 async def json_example():
