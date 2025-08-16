caddd64b0f3712546ccf7c0f62c7890e4032daee
and-rs <deserd@protonmail.com>
2025-08-16 02:39:40 -0500

    refactor: better global template dependency

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
 
 from dotenv import load_dotenv
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 is_dotenv_loaded = load_dotenv()
 print(f"Is dotenv loaded? {is_dotenv_loaded}")
